a968fc425df6d0eb592594339b024ad0e57e0940
Edward Zou <t5957810@gmail.com>
2021-11-21 21:53:57 +0800

    計時器改成rxJS timer, input移除type number

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -3,7 +3,7 @@ import { ModalComponent } from 'src/app/shared/component/modal/modal.component';
 import { DataStorageService } from 'src/app/shared/data-storage.service';
 import { PlaceholderDirective } from 'src/app/shared/directive/placeholder.directive';
 import * as AppConstant from 'src/app/shared/model/app-constant';
-import { Subject } from 'rxjs';
+import { Subject, timer } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
 @Component({
@@ -33,13 +33,13 @@ export class HeaderComponent implements OnInit, OnDestroy {
 
   onFetch() {
     this.dataStorageService.fetchAttractions().subscribe(() => {
-      this.showAlert(this.appConstant.Common.GET_DATA, true);
+      this.showAlert(this.appConstant.Common.GET_DATA);
     }, () => {
       this.showAlert(this.appConstant.ErrorMessage.CORS_ERROR);
     });
   }
 
-  private showAlert(message: string, autoClose = false) {
+  private showAlert(message: string) {
     const alertComponentFactory = this.componentFactoryResolver.resolveComponentFactory(ModalComponent);
     const hostViewContainerRef = this.alertHost.viewContainerRef;
 
@@ -47,11 +47,7 @@ export class HeaderComponent implements OnInit, OnDestroy {
     const componentRef = hostViewContainerRef.createComponent(alertComponentFactory);
     componentRef.instance.errorMessage = message;
 
-    if (autoClose) {
-      setTimeout(() => {
-        hostViewContainerRef.clear();
-      }, 2000);
-    }
+    timer(2000).subscribe(() => hostViewContainerRef.clear());
 
     componentRef.instance.close$.pipe(takeUntil(this.destory)).subscribe(() => {
       hostViewContainerRef.clear();
